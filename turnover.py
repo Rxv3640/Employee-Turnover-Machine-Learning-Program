e63e3c9bda647d5d3532dd118ee7957a8b0d3f57
Rishi Vemireddy <vemireddyrishi@gmail.com>
2026-05-21 15:39:33 -0500

    Continued with employee turnover project

--- turnover.py
+++ turnover.py
@@ -105,7 +105,7 @@ plt.show()
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report, roc_curve, auc
+from sklearn.metrics import classification_report, roc_curve, auc, confusion_matrix, ConfusionMatrixDisplay
 
 
 pipeline = Pipeline([
@@ -163,4 +163,95 @@ print("Cross Validation Scores\n")
 print(cv_scores, "\n")
 
 #Identify the best model and justify the evaluation metrics used.
+
+import numpy as np
+
+#Logistic Regression model
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_test_log_reg)
+
+roc_auc = auc(fpr, tpr)
+
+youden_j = tpr - fpr
+optimal_threshold_index = np.argmax(youden_j)
+optimal_threshold = thresholds[optimal_threshold_index]
+
+plt.figure(figsize=(8,6))
+plt.plot(fpr,tpr, color='blue', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.scatter(fpr[optimal_threshold_index], tpr[optimal_threshold_index], color='red', marker='o', label=f'Optimal Threshold = {optimal_threshold:.4f}')
+plt.plot([0,1], [0,1], color= 'grey', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc='lower right')
+plt.show()
+
+conf_matrix = confusion_matrix(y_test, y_pred_test_log_reg)
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix = conf_matrix)
+
+cm_display.plot()
+plt.show()
+
+
+
+#Random Forest Classifier Model
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_test_rf)
+
+roc_auc = auc(fpr, tpr)
+
+youden_j = tpr - fpr
+optimal_threshold_index = np.argmax(youden_j)
+optimal_threshold = thresholds[optimal_threshold_index]
+
+plt.figure(figsize=(8,6))
+plt.plot(fpr,tpr, color='blue', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.scatter(fpr[optimal_threshold_index], tpr[optimal_threshold_index], color='red', marker='o', label=f'Optimal Threshold = {optimal_threshold:.4f}')
+plt.plot([0,1], [0,1], color= 'grey', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc='lower right')
+plt.show()
+
+conf_matrix = confusion_matrix(y_test, y_pred_test_rf)
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix = conf_matrix)
+
+cm_display.plot()
+plt.show()
+
+#Gradient Boosting Classifier Model
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_test_gb)
+
+roc_auc = auc(fpr, tpr)
+
+youden_j = tpr - fpr
+optimal_threshold_index = np.argmax(youden_j)
+optimal_threshold = thresholds[optimal_threshold_index]
+
+plt.figure(figsize=(8,6))
+plt.plot(fpr,tpr, color='blue', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.scatter(fpr[optimal_threshold_index], tpr[optimal_threshold_index], color='red', marker='o', label=f'Optimal Threshold = {optimal_threshold:.4f}')
+plt.plot([0,1], [0,1], color= 'grey', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc='lower right')
+plt.show()
+
+conf_matrix = confusion_matrix(y_test, y_pred_test_gb)
+
+cm_display = ConfusionMatrixDisplay(confusion_matrix = conf_matrix)
+
+cm_display.plot()
+plt.show()
 
